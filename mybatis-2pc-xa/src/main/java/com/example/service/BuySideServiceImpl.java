b1a7232c5408c69abe589137a3126e8074fc25a0
Hasegawa Yuichi <hase@starlight-storm.com>
2016-12-19 09:49:08 +0900

    importの修正。

--- mybatis-2pc-xa/src/main/java/com/example/service/BuySideServiceImpl.java
+++ mybatis-2pc-xa/src/main/java/com/example/service/BuySideServiceImpl.java
@@ -1,14 +1,11 @@
 package com.example.service;
 
-import java.net.URI;
 import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import com.example.service.domain.Orderer;
 import com.example.service.domain.Product;
